dc84f920c05ec27eb09ff0dfde7250873dffd0e2
Madhumathi <95084888+BMadhumathi@users.noreply.github.com>
2023-02-23 16:22:09 +0530

    Add files via upload

---
+++ EmployeeService.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class EmployeeService {
+
+@Autowired
+
+private Employeerepo emp;
+
+public Employee saveDetails(Employee e) {
+
+return emp.save(e);
+
+}
+
+public List <Employee> getAllDetails(){
+
+return emp.findAll();
+
+}
+
+public void deleteDepartmentById(int id) {
+
+emp.deleteById(id);
+
+}
+
+}
+
